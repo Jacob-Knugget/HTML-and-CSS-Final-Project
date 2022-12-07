cb684c2d34a2f79596dafedebe7a666b587daf69
Jacob-Knugget <120045032+Jacob-Knugget@users.noreply.github.com>
2022-12-07 11:54:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+function bard() {
+    document.getElementById("header-name").innerHTML = "The Bard";
+    document.getElementById("firstLink").innerHTML = "College of Lore";
+    document.getElementById("firstLink").href = "http://dnd5e.wikidot.com/bard:lore";
+
+    document.getElementById("secondLink").innerHTML = "College of Valor";
+    document.getElementById("secondLink").href = "http://dnd5e.wikidot.com/bard:valor";
+
+    document.getElementById("thirdLink").innerHTML = "College of Creation";
+    document.getElementById("thirdLink").href = "http://dnd5e.wikidot.com/bard:creation";
+
+    document.getElementById("fourthLink").innerHTML = "College of Spirits";
+    document.getElementById("fourthLink").href = "http://dnd5e.wikidot.com/bard:spirits";
+
+    document.getElementById("para1").innerHTML = "Whether scholar, skald, or scoundrel, a bard weaves magic through words and music to inspire allies, demoralize foes, manipulate minds, create illusions, and even heal wounds. The bard is a master of song, speech, and the magic they contain.";
+    document.getElementById("head1").innerHTML = "College of Lore:";
+    document.getElementById("para2").innerHTML = "Bards of the College of Lore know something about most things, collecting bits of knowledge from sources as diverse as scholarly tomes and peasant tales.";
+
+    document.getElementById("head2").innerHTML = "College of Valor:";
+    document.getElementById("para3").innerHTML = "Bards of the College of Valor are daring skalds whose tales keep alive the memory of the great heroes of the past, and thereby inspire a new generation of heroes.";
+
+    document.getElementById("head3").innerHTML = "College of Creation:";
+    document.getElementById("para4").innerHTML = "Bards believe the cosmos is a work of art - the creation of the first dragons and gods. That creative work included harmonies that continue to resound through existence today, a power known as the Song of Creation.";
+
+    document.getElementById("head4").innerHTML = "College of Spirits:";
+    document.getElementById("para5").innerHTML = "Bards of the College of Spirits seek tales with inherent power-be they legends, histories, or fictions-and bring their subjects to life.";
+
+    document.getElementById("img1").src = "media/bard3.webp";
+    document.getElementById("img2").src = "media/bard1.jpg";
+
+    document.body.style.background = "url('media/tavern.webp')";
+    document.getElementById("header").style.background = "rgb(105, 43, 24)";
+    document.getElementById("header").style.color = "mediumseagreen";
+    document.getElementById("aside").style.background = "rgb(105, 43, 24)";
+    document.getElementById("nav").style.background = "rgb(105, 43, 24)";
+    document.getElementById("footer").style.background = "rgb(105, 43, 24)";
+    document.getElementById("main").style.background = "peachpuff";
+}
+
+function paladin() {
+    document.getElementById("header-name").innerHTML = "The Paladin";
+    document.getElementById("firstLink").innerHTML = "Oath of the Ancients";
+    document.getElementById("firstLink").href = "http://dnd5e.wikidot.com/paladin:ancients";
+
+    document.getElementById("secondLink").innerHTML = "Oath of Conquest";
+    document.getElementById("secondLink").href = "http://dnd5e.wikidot.com/paladin:conquest";
+
+    document.getElementById("thirdLink").innerHTML = "Oath of Vengeance";
+    document.getElementById("thirdLink").href = "http://dnd5e.wikidot.com/paladin:vengeance";
+
+    document.getElementById("fourthLink").innerHTML = "Oath of the Crown";
+    document.getElementById("fourthLink").href = "http://dnd5e.wikidot.com/paladin:crown";
+
+    document.getElementById("para1").innerHTML = "Whether sworn before a god's altar and the witness of a priest, in a sacred glade before nature spirits and fey beings, or in a moment of desperation and grief with the dead as the only witness, a paladin's oath is a powerful bond.";
+    document.getElementById("head1").innerHTML = "Oath of the Ancients:";
+    document.getElementById("para2").innerHTML = "The Oath of the Ancients is as old as the race of elves and the rituals of the druids. Sometimes called fey knights, green knights, or horned knights, paladins who swear this oath cast their lot with the side of the light in the cosmic struggle against darkness because they love the beautiful and life-giving things of the world, not necessarily because they believe in principles of honor, courage, and justice.";
+
+    document.getElementById("head2").innerHTML = "Oath of Conquest";
+    document.getElementById("para3").innerHTML = "The Oath of Conquest calls to paladins who seek glory in battle and the subjugation of their enemies. It isn’t enough for these paladins to establish order. They must crush the forces of chaos.";
+
+    document.getElementById("head3").innerHTML = "Oath of Vengeance:";
+    document.getElementById("para4").innerHTML = "The Oath of Vengeance is a solemn commitment to punish those who have committed a grievous sin. When evil forces slaughter helpless villagers, when an entire people turns against the will of the gods, when a thieves' guild grows too violent and powerful, when a dragon rampages through the countryside – at times like these, paladins arise and swear an Oath of Vengeance to set right that which has gone wrong.";
+
+    document.getElementById("head4").innerHTML = "Oath of the Crown:";
+    document.getElementById("para5").innerHTML = "The Oath of the Crown is sworn to the ideals of civilization, be it the spirit of a nation, fealty to a sovereign, or service to a deity of law and rulership.";
+
+    document.getElementById("img1").src = "media/paladin1.jpg";
+    document.getElementById("img2").src = "media/paladin2.jpg";
+
+    document.body.style.background = "url('media/castle.jpg')";
+    document.getElementById("header").style.background = "blue";
+    document.getElementById("header").style.color = "Yellow";
+    document.getElementById("aside").style.background = "blue";
+    document.getElementById("nav").style.background = "blue";
+    document.getElementById("footer").style.background = "blue";
+    document.getElementById("main").style.background = "silver";
+}
